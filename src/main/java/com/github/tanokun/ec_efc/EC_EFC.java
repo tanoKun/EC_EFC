63dbc77242ebb4d822e71d4b68c870ce01555c13
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-11-05 22:01:57 +0900

    [add] 初期化

---
+++ src/main/java/com/github/tanokun/ec_efc/EC_EFC.java
@@ -0,0 +1,17 @@
+package com.github.tanokun.ec_efc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EC_EFC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
