d1d22db99ce7d6f20cce478ce8e9def0ed2b586c
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-11-07 21:54:19 +0900

    [add] ページの追加、デザイン

---
+++ src/main/java/com/github/tanokun/ec_efc/data/EnderChest.java
@@ -0,0 +1,116 @@
+package com.github.tanokun.ec_efc.data;
+
+import com.github.tanokun.ec_efc.TaktiePlugin;
+import com.github.tanokun.ec_efc.util.ItemUtils;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.ClickableItem;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.InventoryListener;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.SmartInventory;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.contents.InventoryContents;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.contents.InventoryProvider;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.contents.Pagination;
+import com.github.tanokun.ec_efc.util.smart_inv.inv.contents.SlotIterator;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class EnderChest implements InventoryProvider {
+    private UUID uuid;
+
+    private ArrayList<ArrayList<ItemStack>> enderChestList = new ArrayList<>();
+
+    private InventoryContents contents;
+
+    public EnderChest(UUID uuid) {
+        this.uuid = uuid;
+        for (ArrayList<ItemStack> list : TaktiePlugin.getPlugin().getDataManager().getInitEnderChest().getEnderChestList()) {
+            enderChestList.add(new ArrayList<>(list));
+        }
+    }
+
+    public SmartInventory getInv(){
+        return SmartInventory.builder()
+                .closeable(true)
+                .cancelable(false)
+                .provider(this)
+                .size(4, 9)
+                .title("§7§l" + Bukkit.getOfflinePlayer(uuid).getName() + "'s EnderChest")
+                .id("EnderChest_" + Bukkit.getOfflinePlayer(uuid).getName())
+                .update(false)
+                .listener(getCloseListener())
+                .build();
+    }
+
+    public void init(Player player, InventoryContents contents) {
+        this.contents = contents;
+        contents.fillRow(3, ClickableItem.of(ItemUtils.createItem(Material.BLACK_STAINED_GLASS_PANE, "", 1, false), e -> e.setCancelled(true)));
+
+        Pagination pagination = contents.pagination();
+        ClickableItem[] items = new ClickableItem[this.enderChestList.size() * 27];
+        for (int n = 0; n < enderChestList.size() * 27; n++) items[n] = ClickableItem.empty(enderChestList.get(n / 27).get(n % 27));
+
+        pagination.setItems(items);
+        pagination.setItemsPerPage(27);
+        pagination.addToIterator(contents.newIterator(SlotIterator.Type.HORIZONTAL, 0, 0));
+
+        arrow(player, contents);
+    }
+
+    private void arrow(Player player, InventoryContents contents){
+        Pagination pagination = contents.pagination();
+
+        if (pagination.getPage() == 0)
+            contents.set(3, 0, ClickableItem.of(ItemUtils.createItem(Material.BLACK_STAINED_GLASS_PANE, "", 1, false), e -> e.setCancelled(true)));
+        else
+            contents.set(3, 0, ClickableItem.of(ItemUtils.createItem(Material.SPECTRAL_ARROW,
+                    pagination.getPage() + "ページに戻る", 1, true), e -> {
+                contents.inventory().open(player, pagination.getPage() - 1);
+                TaktiePlugin.playSound(player, Sound.ENTITY_SHULKER_OPEN, 10, 1);
+            }));
+
+        if (pagination.isLast())
+            contents.set(3, 8, ClickableItem.of(ItemUtils.createItem(Material.BLACK_STAINED_GLASS_PANE, "", 1, false), e -> e.setCancelled(true)));
+        else
+            contents.set(3, 8, ClickableItem.of(ItemUtils.createItem(Material.SPECTRAL_ARROW,
+                    (pagination.getPage() + 2) + "ページに進む", 1, true), e -> {
+                contents.inventory().open(player, pagination.getPage() + 1);
+                TaktiePlugin.playSound(player, Sound.ENTITY_SHULKER_OPEN, 10, 1);
+            }));
+    }
+
+    private InventoryListener<InventoryCloseEvent> getCloseListener() {
+        return new InventoryListener<>(InventoryCloseEvent.class, e -> {
+            int p = contents.pagination().getPage();
+            for (int i = 0; i < 27; i++) enderChestList.get(p).set(i, e.getInventory().getContents()[i]);
+            TaktiePlugin.getPlugin().getDataManager().saveEnderChest(this);
+        });
+    }
+
+    public void addPage() {
+        ArrayList<ItemStack> page = new ArrayList<>(); for (int i = 0; i < 27; i++) page.add(new ItemStack(Material.AIR));
+        enderChestList.add(page);
+    }
+
+    public void removePage() {
+        enderChestList.remove(enderChestList.size() - 1);
+    }
+
+    public ArrayList<ArrayList<ItemStack>> getEnderChestList() {
+        return enderChestList;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setEnderChestList(ArrayList<ArrayList<ItemStack>> enderChestList) {
+        this.enderChestList = enderChestList;
+    }
+
+}
