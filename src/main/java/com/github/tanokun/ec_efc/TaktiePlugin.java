564bdf0e5b8a82059e88f8dd3dd6b36423d9fcc4
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2021-11-07 21:55:32 +0900

    [add] Manager、Listener、Commandの追加

--- src/main/java/com/github/tanokun/ec_efc/TaktiePlugin.java
+++ src/main/java/com/github/tanokun/ec_efc/TaktiePlugin.java
@@ -1,7 +1,14 @@
 package com.github.tanokun.ec_efc;
 
+import com.github.tanokun.ec_efc.command.OpenCommand;
+import com.github.tanokun.ec_efc.data.DataManager;
+import com.github.tanokun.ec_efc.data.EnderChest;
+import com.github.tanokun.ec_efc.listener.InitListener;
 import com.github.tanokun.ec_efc.util.command.CommandManager;
 import com.github.tanokun.ec_efc.util.smart_inv.inv.InventoryManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TaktiePlugin extends JavaPlugin {
@@ -10,6 +17,8 @@ public final class TaktiePlugin extends JavaPlugin {
     private InventoryManager inventoryManager;
 
     private CommandManager commandManager;
+
+    private DataManager dataManager;
 
     public static TaktiePlugin getPlugin() {
         return taktie;
@@ -20,16 +29,34 @@ public final class TaktiePlugin extends JavaPlugin {
         taktie = this;
 
         registryManagers();
+        registryCommands();
+        registryListeners();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            EnderChest enderChest = TaktiePlugin.getPlugin().getDataManager().loadEnderChest(player.getUniqueId());
+            TaktiePlugin.getPlugin().getDataManager().addEnderChest(player.getPlayer().getUniqueId(), enderChest);
+        }
     }
 
     @Override
     public void onDisable() {
+
     }
 
     private void registryManagers(){
         this.inventoryManager = new InventoryManager(this);
 
         this.commandManager = new CommandManager(this);
+
+        this.dataManager = new DataManager();
+    }
+
+    private void registryCommands() {
+        commandManager.registerCommand(new OpenCommand());
+    }
+
+    private void registryListeners() {
+        Bukkit.getPluginManager().registerEvents(new InitListener(), this);
     }
 
     public InventoryManager getInventoryManager() {
@@ -39,4 +66,13 @@ public final class TaktiePlugin extends JavaPlugin {
     public CommandManager getCommandManager() {
         return commandManager;
     }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public static void playSound(Player player, Sound sound, int volume, double v2) {
+        player.playSound(player.getLocation(), sound, volume, (float) v2);
+    }
+
 }
